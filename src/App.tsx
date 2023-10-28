b0118d34c9b78e17444e87954193a7a434405f18
priyanshu naithani <naithanip000@gmail.com>
2023-10-28 19:04:37 +0530

    add something about auther in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>HEllo I'm Priyanshu Naithani.</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
